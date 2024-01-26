bb03327cc99878df6f2cf28c030c1aa7ee9cdcf1
VanTu <tulevan526@gmail.com>
2024-01-26 22:40:07 +0700

    hello world

--- demoGit/demoGit/Program.cs
+++ demoGit/demoGit/Program.cs
@@ -9,11 +9,11 @@ namespace demoGit
 {
     internal class Program
     {
-        int a = 3000;
-        int b = 3000;
+        int c = 3000;
+        int d = 3000;
         string name = "123 bon ngay";
         string newOne = " nhom12345";
-
+        string temp = "temp";
         static void Main(string[] args)
         {
             
