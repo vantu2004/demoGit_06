17378e08858b025fb98e85e64b08da1b10ad2763
TieuNhi <TrVuKhah27@gmail.com>
2024-01-26 22:49:03 +0700

    new

--- demoGit/demoGit/Program.cs
+++ demoGit/demoGit/Program.cs
@@ -16,6 +16,9 @@ namespace demoGit
 
         string l10 = "A o dau ta ban chieu gon,\r\nChang hay chieu ay het hay con.";
         string l11 = "Xuan thu phong do chung bao tuoi,\r\nDa co chong chua, duoc may con?";
+
+        string l12 = "Toi o Tay Ho ban chieu gon,\r\nCan chi ong hoi het hay con.";
+        string l13 = "Xuan thu phong do trang tron le,\r\nChong con chua co, co chi con!";
         static void Main(string[] args)
         {
             
