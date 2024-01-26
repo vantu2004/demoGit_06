5fc54e424b46cd61006dee8251cd543a8eca6d80
TieuNhi <TrVuKhah27@gmail.com>
2024-01-26 22:24:31 +0700

    Gheo co hang chieu

--- demoGit/demoGit/Program.cs
+++ demoGit/demoGit/Program.cs
@@ -11,11 +11,16 @@ namespace demoGit
     {
         int a = 12;
         int b = 18;
+        int c = 30;
         string l0 = "A o dau ta ban chieu gon";
         string l1 = "Chang hay chieu ay het hay con";
         string l2 = "Xuan thu phong do chung bao tuoi";
         string l3 = "Da co chong chua, duoc may con?";
 
+        string l4 = "Toi o Tay Ho ban chieu gon";
+        string l5 = "Can chi ong hoi het hay con.";
+        string l6 = "Xuan thu phong do trang tron le";
+        string l7 = "Chong con chua co, co chi con!";
 
         static void Main(string[] args)
         {
